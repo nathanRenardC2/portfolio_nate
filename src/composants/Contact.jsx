a354841e2ec7154bbaeefe91d80d70b6d4054ecc
NathanrenardC2 <nathan.renard@etu.univ-grenoble-alpes.fr>
2022-10-14 14:37:52 +0200

    erreur de la console enlever

--- src/composants/Contact.jsx
+++ src/composants/Contact.jsx
@@ -3,23 +3,23 @@ import { useState, useEffect } from "react";
 
 export default function Contact() {
     return(
-        <form class="w-full max-w-sm mb-10">
-            <div class="flex items-center border-b border-slate-300 py-2 mb-5">
-                <input class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-semibold" type="text" placeholder="Pseudonyme *" aria-label="Full name" required="required"></input>
+        <form className="w-full max-w-sm mb-10">
+            <div className="flex items-center border-b border-slate-300 py-2 mb-5">
+                <input className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-semibold" type="text" placeholder="Pseudonyme *" aria-label="Full name" required="required"></input>
             </div>
-            <div class="flex items-center border-b border-slate-300 py-2 mb-5">
-                <input class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-semibold" type="text" placeholder="Nom" aria-label="Full name"></input>
+            <div className="flex items-center border-b border-slate-300 py-2 mb-5">
+                <input className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-semibold" type="text" placeholder="Nom" aria-label="Full name"></input>
             </div>
-            <div class="flex items-center border-b border-slate-300 py-2 mb-5">
-                <input class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-semibold" type="text" placeholder="Prenom" aria-label="Full name"></input>
+            <div className="flex items-center border-b border-slate-300 py-2 mb-5">
+                <input className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-semibold" type="text" placeholder="Prenom" aria-label="Full name"></input>
             </div>
-            <div class="flex items-center border-b border-slate-300 py-2 mb-5">
-                <input class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-semibold" type="text" placeholder="Email *" aria-label="Full name" required="required"></input>
+            <div className="flex items-center border-b border-slate-300 py-2 mb-5">
+                <input className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-semibold" type="text" placeholder="Email *" aria-label="Full name" required="required"></input>
             </div>
-            <div class="flex items-center border-b border-slate-300 pb-20 mb-5">
-                <input class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-semibold" type="text" placeholder="Message" aria-label="Full name"></input>
+            <div className="flex items-center border-b border-slate-300 pb-20 mb-5">
+                <input className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none font-semibold" type="text" placeholder="Message" aria-label="Full name"></input>
             </div>
-            <div class="flex items-center justify-center">
+            <div className="flex items-center justify-center">
                 <button className="text-white font-bold flex text-xl bg-[#212E43] p-2 rounded-md">
                     Envoyer
                 </button>
--- src/composants/Navbar.jsx
+++ src/composants/Navbar.jsx
@@ -27,26 +27,26 @@ export default function Navbar() {
     }
 
     return(
-        <nav class="bg-[#212E43] border-gray-200 px-2 sm:px-4 py-5">
-            <div class="container flex flex-wrap justify-between items-center mx-auto">
-                <a href="" class="flex items-center ml-4">
-                    <span class="text-white self-center text-xl font-bold whitespace-nowrap md:text-2xl">Nateraki</span>
+        <nav className="bg-[#212E43] border-gray-200 px-2 sm:px-4 py-5">
+            <div className="container flex flex-wrap justify-between items-center mx-auto">
+                <a href="" className="flex items-center ml-4">
+                    <span className="text-white self-center text-xl font-bold whitespace-nowrap md:text-2xl">Nateraki</span>
                 </a>
-                <button onClick={handleToggle} data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-4 mr-4 text-sm text-white rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                <button onClick={handleToggle} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-4 mr-4 text-sm text-white rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                 </button>
-                {navbarOpen ||largeur > 768 ? (
-                    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                        <ul class="flex flex-col p-4 mt-4 rounded-lg text-white border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:border-gray-700">
-                            <li class="">
-                                <a href="#" class="block py-2 pr-4 pl-3 md:bg-transparent md:hover:text-gray-400 md:p-0 dark:text-white md:text-lg md:mr-10 md:font-bold" aria-current="page">A propos</a>
+                {!navbarOpen ||largeur > 768 ? (
+                    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                        <ul className="flex flex-col p-4 mt-4 rounded-lg text-white border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:border-gray-700">
+                            <li className="">
+                                <a href="#" className="block py-2 pr-4 pl-3 md:bg-transparent md:hover:text-gray-400 md:p-0 dark:text-white md:text-lg md:mr-10 md:font-bold" aria-current="page">A propos</a>
                             </li>
                             <li>
-                                <a href="#" class="block py-2 pr-4 pl-3 md:hover:text-gray-400 md:p-0 dark:text-gray-400 md:dark:hover:text-white  md:text-lg md:mr-10 md:font-bold dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projets</a>
+                                <a href="#" className="block py-2 pr-4 pl-3 md:hover:text-gray-400 md:p-0 dark:text-gray-400 md:dark:hover:text-white  md:text-lg md:mr-10 md:font-bold dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projets</a>
                             </li>
                             <li>
-                                <a href="#" class="block py-2 pr-4 pl-3 md:hover:text-gray-400 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700  md:mr-10 md:text-lg md:font-bold dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                                <a href="#" className="block py-2 pr-4 pl-3 md:hover:text-gray-400 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700  md:mr-10 md:text-lg md:font-bold dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
                             </li>
                         </ul>
                     </div>) 
